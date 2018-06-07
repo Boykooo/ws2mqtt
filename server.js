db1205eef623d601cc850222214f60993243f5d2
aboytsov <andrey.b@just.ru.com>
2018-06-07 15:56:34 +0300

    Impl WS to MQTT translator

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ http.listen(4201, function () {
     console.log('listening on *:4201');
 });
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', function (socket) {
     console.log('a user connected');
     socket.on('connect-to-mqtt', function (data) {
@@ -23,7 +19,29 @@ var mqttClient;
 function connectToMqtt(data, socket) {
     console.log('Connection data: ' + JSON.stringify(data));
     mqttClient = mqtt.connect('mqtt://localhost:7707', {username: data.username, password: data.password});
+
+    // notify client about mqtt connection
     mqttClient.on('connect', function () {
         socket.emit('mqtt-connection/' + data.username);
+        handleMqttSub(socket);
+    });
+}
+
+function handleMqttSub(socket) {
+
+    // redirect from SOCKET to MQTT
+    socket.on('mqtt-sub', function (data) {
+        console.log(data);
+        var topic = data.topic;
+        var payload = data.payload;
+        console.log('Send to MQTT: topic = ' + topic + ' with payload = ' + JSON.stringify(payload));
+        mqttClient.subscribe(topic);
+        // mqttClient.publish(topic, JSON.stringify(payload));
+    });
+
+    // redirect from MQTT to SOCKET
+    mqttClient.on('message', function (topic, message) {
+        console.log('Send to SOCKET: topic = ' + topic + ' with payload = ' + JSON.stringify(message));
+        socket.emit(topic, message);
     })
 }
